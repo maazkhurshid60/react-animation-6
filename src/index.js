06a58781df562eb71047d47a2cf117532f9eb1f6
maazkhurshid60 <maaz.khurshid60@gmail.com>
2024-07-03 17:16:47 +0500

    added changes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import PrizeArcade from './Prize-Arcade';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// adding changes for git activity
+// again adding changes for git activity
 root.render(
   <React.StrictMode>
     <PrizeArcade />
