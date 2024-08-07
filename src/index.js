f0db8c6d6ee27f81020b77dbf35a22fcdfbed29a
Noor ur Rehman <135664580+TheRehmandev@users.noreply.github.com>
2024-08-07 23:23:23 +0500

    Commiting changes for git activity

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import PrizeArcade from './Prize-Arcade';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// adding this line git activity
+// changing this line git activity
 root.render(
   <React.StrictMode>
     <PrizeArcade />
