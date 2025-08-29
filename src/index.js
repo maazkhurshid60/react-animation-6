60676ef6eec2d0d2644a8d98f659a6c1c6791359
maazkhurshid60 <maaz.khurshid60@gmail.com>
2025-08-29 17:53:20 +0500

    Git activity performed

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import PrizeArcade from './Prize-Arcade';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-//deleting this line for git activity
+//Adding this line for git activity
 root.render(
   <React.StrictMode>
     <PrizeArcade />
